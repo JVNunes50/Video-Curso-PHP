63b6c2237b7adb548382c1287da1a28d1e088577
JVNunes50 <jvnunes1944@gmail.com>
2025-06-01 22:07:04 -0300

    Correção da formatação da média e do calculo

--- Desafios/d09/index.php
+++ Desafios/d09/index.php
@@ -35,10 +35,10 @@
         <h1>Calculo das Médias</h1>
         <?php 
             $mediaSimples = ($valor1 + $valor2) / 2;
-            $mediaPonderada = (($peso1 * $valor1) + ($peso2 * $valor2)) / ($peso1 + $peso2);
+            $mediaPonderada = ($peso1 * $valor1 + $peso2 * $valor2) / ($peso1 + $peso2);
 
             echo "Analisando os valores de $valor1 e $valor2";
-            echo "<ul><li>A Médias Aritméticas Simples entre os valores é igual a $mediaSimples</ul></li>";
+            echo "<ul><li>A Médias Aritméticas Simples entre os valores é igual a " . number_format($mediaSimples, 2, ',') . "</ul></li>";
             echo "<ul><li> Médias Aritméticas Ponderada com pesos 1 e 5 é igual a " . number_format($mediaPonderada, 2 , ',') . "</ul></li>"
         ?>
     </section>
